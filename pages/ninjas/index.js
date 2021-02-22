adfcc7e2f3068d2bf1d1b54fcb9600fca5d2a836
Kioshi Okamoto <kjor29@gmail.com>
2021-02-22 15:51:03 -0500

    :package: NEW: Next.js - crash course: net ninja

---
+++ pages/ninjas/index.js
@@ -0,0 +1,30 @@
+import styles from '../../styles/Ninjas.module.css'
+import Link from 'next/link'
+export const getStaticProps = async ()=>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props:{ ninjas:data }
+    }
+}
+
+
+const ninjas = ({ninjas}) => {
+	return (
+        <div>
+            <h1>All ninjas</h1>
+            {ninjas.map((ninja)=>(
+                <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+                    <a className={styles.single}>
+                        <h3>{ninja.name}</h3>
+                    </a>
+                </Link>
+            ))}
+        </div>
+    );
+};
+
+export default ninjas;
+
+
---
+++ pages/productos/productos.js
@@ -0,0 +1,17 @@
+/* function Productos() {
+    return (
+      <>
+        <h1>asa</h1>
+      </>
+    )
+  }
+   
+  export default Productos */
+
+export default function Productos() {
+	return (
+		<div>
+			<h1>Productos</h1>
+		</div>
+	);
+}
